80c10b9f4d38d3965fcbee1ec67861b86294960e
Romeo Jeremiah <romeoclayton@gmail.com>
2022-03-02 19:20:53 -0500

    Delete javascript basics directory

--- javascript basics/variable_challenge.js
+++
@@ -1,9 +0,0 @@
-let firstName;
-let last_name;
-firstName = "Romeo";
-last_name = "The Coder";
-console.log(firstName, last_name);
-let address = "main street";
-console.log(address);
-address = "first street";
-console.log(address);
