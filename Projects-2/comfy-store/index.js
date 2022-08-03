5d0fd33ed210e12013a72909b5b1bc264b8edbcc
romeojeremiah <romeoclayton@gmail.com>
2022-08-03 18:44:37 -0400

    comfy project beginning

---
+++ Projects-2/comfy-store/index.js
@@ -0,0 +1,9 @@
+// global imports -- used in all the pages
+import "./src/toggleSidebar.js";
+import "./src/cart/toggleCart.js";
+import "./src/cart/setupCart.js";
+// specific imports
+import fetchProducts from "./src/fetchProducts.js";
+import { setupStore, store } from "./src/store.js";
+import display from "./src/displayProducts.js";
+import { getElement } from "./src/utils.js";
